398f82e6cbb93b2cd8f0bda1afac984ad68c2f69
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-05-16 00:38:00 +0200

    List.js

--- components/List.js
+++ components/List.js
@@ -1,48 +1,27 @@
 import React from "react";
-// import List from "../con"
-import {useSelector} from "react-redux"
-import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
-const List = ( {todos, toggleTodo, remove } ) => {
-
-    const todos1 = useSelector(state => state.todos)
+const List = ({ toggleTodo, remove }) => {
+    const todos1 = useSelector((state) => state.todos);
 
     return (
         <div>
-        <h1>To-Do List:</h1>
-        <ul>
-            {todos1.map((todo, index) => (
-                <li
-                    key={index}
-                    onClick={() => toggleTodo(todo.title)}
-                    style={{ textDecoration: todo.done ? "line-through" : "none"}}
-                >
-                    <span>{todo.title}</span>
-                    <button onClick={() => remove(todo.title)}>usun</button>
-                    {/*<button onClick={() => remove2s(todo.title)}>usun po 2s</button>*/}
-                    <button>usun po 2s</button>
-                </li>
-            ))}
-
-
-        </ul>
+            <h1>To-Do List:</h1>
+            <ul>
+                {todos1.map((todo, index) => (
+                    <li
+                        key={index}
+                        onClick={() => toggleTodo(todo.title)}
+                        style={{ textDecoration: todo.done ? "line-through" : "none" }}
+                    >
+                        <span>{todo}4</span>
+                        <button onClick={() => remove(todo.title)}>usun</button>
+                        <button>usun po 2s</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
 
-
-
-List.propTypes = {
-    todos: PropTypes.arrayOf(
-        PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            done: PropTypes.bool.isRequired,
-        })
-    ).isRequired,
-    toggleToggle: PropTypes.func.isRequired,
-    remove: PropTypes.func.isRequired,
-};
-
-
 export default List;
-
