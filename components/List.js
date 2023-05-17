49304381425d649eca5e63213b488003bed92b79
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-05-17 02:14:13 +0200

    List.js

--- components/List.js
+++ components/List.js
@@ -1,27 +1,32 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../redux/actions";
 
-const List = ({ toggleTodo, remove }) => {
-    const todos1 = useSelector((state) => state.todos);
+const List = ({ toggleTodo }) => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-    return (
-        <div>
-            <h1>To-Do List:</h1>
-            <ul>
-                {todos1.map((todo, index) => (
-                    <li
-                        key={index}
-                        onClick={() => toggleTodo(todo.title)}
-                        style={{ textDecoration: todo.done ? "line-through" : "none" }}
-                    >
-                        <span>{todo}4</span>
-                        <button onClick={() => remove(todo.title)}>usun</button>
-                        <button>usun po 2s</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const handleRemove = (title) => {
+    dispatch(removeTodo(title));
+  };
+
+  return (
+    <div>
+      <h1>To-Do List:</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            onClick={() => toggleTodo(todo.title)}
+            style={{ textDecoration: todo.done ? "line-through" : "none" }}
+          >
+            <span>{todo.title}</span>
+            <button onClick={() => handleRemove(todo.title)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default List;
