646b5cee309f9df17ccf678281f16340dd383bea
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-05-18 01:27:25 +0200

    reducer.js

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,36 +1,41 @@
 import { combineReducers } from "redux";
 
-const initialState = {
-  todos: []
-}
+const initialState = [];
+
+const initialListState = []
+
+// const list = (initialListState, action) => {
+//   if (action.type === "REMOVE_TODO") {
+//
+//   }
+// }
 
 const todosReducer = (state = initialState, action) => {
   if (action.type === "ADD_TODO") {
-    return {
-      ...state,
-      todos: [...state.todos, action.payload],
-    };
+    return [...state, action.payload];
   } else if (action.type === "TODO_TOGGLED") {
-    return {
-      ...state,
-      todos: state.todos.map((todo) =>
-        todo.title === action.payload ? { ...todo, done: !todo.done } : todo
-      ),
-    };
+    return state.map((todo) =>
+        todo.title === action.payload ? {...todo, done: !todo.done} : todo
+    );
   } else if (action.type === "REMOVE_TODO") {
-    return {
-      ...state,
-      todos: state.todos.filter((todo) => todo.title !== action.payload),
-    };
+    const copy = [...state];
+    copy.splice(action.index, 1);
+    return copy;
   }
-  
   return state;
 };
 
+  // stare rozwiazanie
+  //
+  //   return state.filter((todo) => todo.title !== action.payload);
+  // }
+//
+//   return state;
+// };
+
 export default combineReducers({
-  todos :todosReducer,
+  todos : todosReducer,
   // list,
 });
 
 // export default todosReducer;
-
