180c9f7d806850497b756cfe8ae33b00ea023ff3
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-05-22 23:57:00 +0200

    Actions.js

--- redux/actions.js
+++ redux/actions.js
@@ -1,28 +1,38 @@
 export const ADD_TODO = "ADD_TODO"
 export const TOGGLE_TODO = "TOGGLE_TODO"
 export const REMOVE_TODO = "REMOVE_TODO"
+export const REMOVE_TODO_2S = "REMOVE_TODO_2S"
 
 // export const REMOVE_TODO_2S = "REMOVE_TODO_2S"
 
-export const addTodo = (todoName) => ({
-    type: ADD_TODO,
-    todoName,
-});
+// export const addTodo = (todoName) => {
+//     return {
+//         type: ADD_TODO,
+//         payload: todoName,
+//     };
+// };
+
+export const addTodo = (todoName) => {
+    return {
+        type: ADD_TODO,
+        payload: todoName,
+    };
+};
+
 
 export const toggle = (title) => ({
     type: TOGGLE_TODO,
     payload: title,
 })
 
-export const remove = (title) => ({
+export const removeTodo = (title) => ({
     type: REMOVE_TODO,
     payload: title,
 })
 
-// export const remove_2s = (title) => ({
-//     type: REMOVE_TODO_2S,
-//     // payload: title,
-// })
+export const removeTodo2s= (dispatch, title) => {
+    const id = setInterval(() =>{
+        dispatch(removeTodo());
+    }, 2000);
 
-
-
+}
