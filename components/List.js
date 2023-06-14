d07de8a1d3ba4b43ab8a940a1b2a016688b6b75f
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-06-14 23:38:18 +0200

    Update List.js

--- components/List.js
+++ components/List.js
@@ -4,26 +4,19 @@ import { removeTodo } from "../redux/actions";
 
 const List = ({ toggleTodo, removeTodo, removeTodo2s }) => {
     const todos = useSelector((state) => state.todos);
-    // const dispatch = useDispatch();
-    //
-    // const handleRemove = (title) => {
-    //     dispatch(removeTodo(title));
-    // };
 
     return (
         <div>
             <h1>To-Do List:</h1>
             <ul>
-                {todos.map((todo, index) => (
+                {todos.map((todo, index, li) => (
                     <li
-                        key={index}
-                        onClick={() => toggleTodo(todo.title)}
+                        key={todo.id}                        
                         style={{ textDecoration: todo.done ? "line-through" : "none" }}
                     >
-                        <span>{todo}</span>
-                        {/*<button onClick={() => handleRemove(todo)}>Remove</button>*/}
-                        <button onClick={() => removeTodo(index)}>Remove</button>
-                        <button onClick={() => removeTodo2s(index)}>Remove after 2s</button>
+                        <span onClick={() => toggleTodo(todo.title)}>{todo}</span>                      
+                        <button onClick={() => removeTodo(index)} key={todo.id}>Remove</button>
+                        <button onClick={() => removeTodo2s(index)} key={todo.id}>Remove after 2s</button>
                     </li>
                 ))}
             </ul>
